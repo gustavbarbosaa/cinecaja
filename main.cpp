5b46d31218f9a0495cec9eb20ef1d8076ea29c30
gustavbarbosaa <gustavoaraujohab@gmail.com>
2022-09-20 17:40:57 -0300

    condição de parada criada

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <time.h>
 using namespace std;
 
-void nomeCliente(string name) {
+string nomeCliente(string name) {
     cout << "Seja bem vindo ao CineCajá!" << endl;
     cout << "------------------------------------" << endl;
 
@@ -13,6 +13,8 @@ void nomeCliente(string name) {
     cout << "------------------------------------" << endl;
 
     cout << "Seja bem vindo(a), " << name << "." << endl;
+
+    return name;
 }
 
 int numEntradas(int quantidade) {
@@ -32,81 +34,93 @@ int numeroBilhete(int sort) {
     return sort;
 }
 
+void mostrarArray(int array[][10]) {
+    for (int i = 0; i < 7; i++) {
+        for(int j = 0; j < 10; j++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main() {
 
     bool condicao = true;
     int lugares[7][10] = {0};
-    int contador = 1;
 
-     for (int i = 0; i < 7; i++) {
-        for (int j = 0; j < 10; j++) {
-            lugares[i][j] = contador++;
+    for (int i = 1; i < 7; i++) {
+        for (int j = 1; j < 10; j++) {
+            while (lugares[i][j] == 0) {
+                string setName;
+                string nome = nomeCliente(setName);
+
+                int numBilhete, quantidadeIngressos;
+                float valorEntrada = 12.50;
+
+                cout << "O valor da entrada é: R$" << valorEntrada << ". Certo?" << endl;
+
+                int quantidade = numEntradas(quantidadeIngressos);
+                int sort = numeroBilhete(numBilhete);
+
+                cout << "Os lugares disponiveis são os seguintes: " << endl;
+
+                mostrarArray(lugares);
+
+                cout << "------------------------------------" << endl;
+                int coluna, fileira;
+                cout << "Escolha a coluna (1 a 7): ";
+                cin >> coluna;
+                cout << endl;
+                cout << "Agora escolha a fileira (1 ao 10): ";
+                cin >> fileira;
+
+                cout << "------------------------------------" << endl;                
+
+                while (lugares[coluna - 1][fileira -1] != 0) {
+                    cout << "Este lugar já está ocupado, tente outro!" << endl;
+                    cout << "Informe a coluna (1 a 7): ";
+                    cin >> coluna;
+                    cout << endl;
+                    cout << "Agora escolha a fileira (1 ao 10): ";
+                    cin >> fileira;
+                }
+                
+                lugares[coluna - 1][fileira -1] = sort;
+
+                if (quantidade != 1) {
+                    for (int i = 1; i < quantidade; i++) {
+                        cout << "Escolha seu outro lugar: " << endl;
+                        cout << "Informe a coluna (1 a 7): ";
+                        cin >> coluna;
+                        cout << endl;
+                        cout << "Agora escolha a fileira (1 ao 10): ";
+                        cin >> fileira;
+                        lugares[coluna - 1][fileira -1] = sort;
+                    }
+                }
+                
+                mostrarArray(lugares);
+
+                cout << "------------------------------------" << endl;
+
+                char confirm;
+                float valorTotal = valorEntrada * quantidade;
+
+                cout << "O valor total é: R$" << valorTotal << ". Certo?" << endl;
+                cout << "(Digite 'S' para confirmar ou 'N' para negar) -> ";
+                cin >> confirm;
+
+                if (confirm == 'S' || confirm == 's') {
+                    cout << "Bom filme!" << endl;
+                } else if (confirm == 'N' || confirm == 'n')  {
+                    cout << "Certo, muito obrigado!" << endl;
+                }
+
+                cout << "Certo " << nome << ". Sua ficha é a de número: " << sort << "." << endl;
+                cout << "------------------------------------" << endl;
+            }
         }
     }
-
-    while (condicao) {
-        string name;
-        nomeCliente(name);
-
-        int numBilhete, quantidadeIngressos;
-        float valorEntrada = 12.50;
-        cout << "O valor da entrada é: R$" << valorEntrada << ". Certo?" << endl;
-        int quantidade = numEntradas(quantidadeIngressos);
-        float valorTotal = valorEntrada * quantidade;
-
-        int sort = numeroBilhete(numBilhete);
-
-        cout << "Os lugares disponiveis são os seguintes: " << endl;
-
-        for (int i = 0; i < 7; i++) {
-        for (int j = 0; j < 10; j++) {
-            if (contador <= 10) { 
-                cout << "0" << lugares[i][j] << " ";
-            } else {
-                cout << lugares[i][j] << " ";
-            }
-        }
-        cout << endl;
-        }
-        cout << "------------------------------------" << endl;
-
-        int cadeira;
-        cout << "Escolha o sua cadeira: ";
-        cin >> cadeira;
-
-        for (int i = 0; i < 7; i++) {
-            for(int j = 0; j < 10; j++) {
-                if (lugares[i][j] == cadeira) {
-                    lugares[i][j] = sort;
-                }
-
-                if (contador <= 10) { 
-                    cout << "0" << lugares[i][j] << " ";
-                } else {
-                    cout << lugares[i][j] << " ";
-                }
-            }
-            cout << endl;
-        }
-
-        char confirm;
-        cout << "O valor total é: R$" << valorTotal << ". Certo? (Digite 'S' para confirmar ou 'N' para negar -> ";
-        cin >> confirm;
-
-        if (confirm == 'S' || confirm == 's') {
-            cout << "Bom filme!" << endl;
-        } else if (confirm == 'N' || confirm == 'n')  {
-            cout << "Certo, muito obrigado!" << endl;
-        }
-
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (lugares[i][j] > 70) {
-                    condicao = true;
-                }
-            }
-        }
-    }
+    
     return 0;
 }
